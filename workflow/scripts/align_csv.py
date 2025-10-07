c5bd89571d342fa47a74de58c0ce4dc45c6a6e2f
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2025-10-07 16:59:07 +0200

    update align_csv

--- workflow/scripts/align_csv.py
+++ workflow/scripts/align_csv.py
@@ -16,21 +16,16 @@ def yaml_to_dataframe(filepath):
     df = pd.DataFrame(rows)
     return df
 
-def count_mapped_unmapped_reads(bam_file_path):
-    with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
-        total_reads = bam_file.count(until_eof=True)
-        mapped_reads = bam_file.count()  # Default counts mapped reads only
-        unmapped_reads = total_reads - mapped_reads
-    return mapped_reads, unmapped_reads
-
-
 def main(fnames, bams, fname_out):
     tmp = []
     for fname, bam in zip(fnames, bams):
         df = yaml_to_dataframe(fname)
-        mapped, unmapped = count_mapped_unmapped_reads(bam)
-        df['mapped'] = mapped
-        df['unmapped'] = unmapped
+        # reference sequence name, sequence length, # mapped read-segments and # unmapped read-segments
+        idxstats = pysam.idxstats(bam).split("\t") # ex. 'PQ268485\t7135\t13001354\t0\n*\t0\t0\t0\n'
+        df['reference_sequence_name'] = idxstats[0]
+        df['sequence_length'] = idxstats[1]
+        df['mapped_reads'] = idxstats[2]
+        df['unmapped_reads'] = idxstats[3]
         tmp.append(df)
 
     pd.concat(tmp).to_csv(fname_out)
