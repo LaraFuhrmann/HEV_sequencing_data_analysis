6886d601896b665d5341427202d7cb9763dfcb1d
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2025-10-07 10:19:48 +0200

    add align_csv

---
+++ workflow/scripts/align_csv.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import re
+import pysam
+import yaml
+
+def yaml_to_dataframe(filepath):
+    with open(filepath, 'r') as file:
+        yaml_data = yaml.safe_load(file)
+
+    rows = []
+    for sample_name, values in yaml_data.items():
+        row = {'sample_name': sample_name}
+        row.update(values)
+        rows.append(row)
+
+    df = pd.DataFrame(rows)
+    return df
+
+def count_mapped_unmapped_reads(bam_file_path):
+    with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
+        total_reads = bam_file.count(until_eof=True)
+        mapped_reads = bam_file.count()  # Default counts mapped reads only
+        unmapped_reads = total_reads - mapped_reads
+    return mapped_reads, unmapped_reads
+
+
+def main(fnames, bams, fname_out):
+    tmp = []
+    for fname, bam in zipped(fnames, bams):
+        df = yaml_to_dataframe(fname)
+        mapped, unmapped = count_mapped_unmapped_reads(bam)
+        df['mapped'] = mapped
+        df['unmapped'] = unmapped
+
+    pd.concat(tmp).to_csv(fname_out)
+
+if __name__ == "__main__":
+    main(
+        snakemake.input.fnames,
+        snakemake.input.bams,
+        snakemake.output.fname,
+    )
